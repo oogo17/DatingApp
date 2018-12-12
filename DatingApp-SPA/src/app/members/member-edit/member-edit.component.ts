a2660e9b299d80e59d7888f9bf9c6d8a5d10c91e
Omar Gonzalez <Omar_Gonzalez@epam.com>
2018-12-12 16:21:10 -0600

    Using BehaviorSubject to comunicate any acomponent

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -15,6 +15,7 @@ export class MemberEditComponent implements OnInit {
 @ViewChild('editForm') editForm: NgForm;
 
 user: User;
+photoUrl: string;
 @HostListener('window:beforeunload', ['$event'])
 unloadNotification($event: any){
   if (this.editForm.dirty) {
@@ -29,6 +30,7 @@ unloadNotification($event: any){
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+    this.authService.currentPhotoUrl.subscribe(photoUrl => this.photoUrl = photoUrl);
   }
 
   updateUser () {
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -10,9 +10,11 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
   model: any = {};
+  photoUrl: string;
   constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
+    this.authService.currentPhotoUrl.subscribe(photoUrl => this.photoUrl = photoUrl);
   }
 
   login() {
